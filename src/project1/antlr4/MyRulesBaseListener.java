577414b893238f8955209f227ca9b64ad047e63c
laurenruffner <lauren.ruffner@yahoo.com>
2019-09-25 14:15:50 -0500

    Phase 2 Part 2 Prep

--- src/project1/antlr4/MyRulesBaseListener.java
+++ src/project1/antlr4/MyRulesBaseListener.java
@@ -2,13 +2,14 @@ package project1.antlr4;
 import project1.Dbms;
 
 public class MyRulesBaseListener extends RulesBaseListener{
+    Dbms myDbms;
     //default constructor
     public MyRulesBaseListener() {
         myDbms = new Dbms();
     }
-//    @Override public void exitShowCmd(RulesParser.ShowCmdContext ctx) {
-//        System.out.println("SHOW");
-//    }
+    @Override public void exitShow_cmd(RulesParser.Show_cmdContext ctx){
+        System.out.println ("SHOW");
+    }
 
 }
 
